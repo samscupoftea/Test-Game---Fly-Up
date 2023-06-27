950cae4583860d7988ae29c84e6abca1f807f34a
sam hockaday <samuel.hockaday@gmail.com>
2023-06-27 10:26:50 +0100

    Created canvas gamespace

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
 canvas.width = 650;
-canvas.height = 800;
+canvas.height = 1000;
 
 c.fillRect(0, 0, canvas.width, canvas.height);
