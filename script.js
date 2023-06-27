4498601612a8d95a63e1e8fa3ccf75551a1000b1
sam hockaday <samuel.hockaday@gmail.com>
2023-06-27 14:27:52 +0100

    Trying to add eventlistener to gap

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
+// const canvas = document.querySelector('canvas')
+// const c = canvas.getContext('2d')
 
-function Bar() {
-    this.left = random(width / 2);
-    this.right = random(width / 2);
-    this.x = height;
-    this.w = 20;
+const block = document.getElementById("block");
+const gap = document.getElementById("gap");
+
+gap.addEventListener('animationiteration'), () => {
+    let random = (Math.random() * 300) + 150);
+
 }
+
+
+
+
+
+
+// function Bar() {
+//     this.left = random(width / 2);
+//     this.right = random(width / 2);
+//     this.x = height;
+//     this.w = 20;
+// }
 
 
 
