550261d3c338791718c28c390824f90d15c9274e
sam hockaday <samuel.hockaday@gmail.com>
2023-06-27 10:18:37 +0100

    Removed some elements

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-canvas.width = 800
-canvas.height = 1000
+canvas.width = 650;
+canvas.height = 800;
 
-c.fillRect(0, 0, canvas.width, canvas.height)
+c.fillRect(0, 0, canvas.width, canvas.height);
