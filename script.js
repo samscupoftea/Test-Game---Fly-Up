7522d20835bcb5e0b600206a5ed74e2f3866282f
sam hockaday <samuel.hockaday@gmail.com>
2023-06-27 14:13:36 +0100

    JS

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
+function Bar() {
+    this.left = random(width / 2);
+    this.right = random(width / 2);
+    this.x = height;
+    this.w = 20;
+}
 
 
