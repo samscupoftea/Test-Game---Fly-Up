bcf93181d06f861e07d87bfe2964177b1503258f
sam hockaday <samuel.hockaday@gmail.com>
2023-06-27 10:29:26 +0100

    Cavas size

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
 canvas.width = 650;
-canvas.height = 1000;
+canvas.height = 750;
 
 c.fillRect(0, 0, canvas.width, canvas.height);
