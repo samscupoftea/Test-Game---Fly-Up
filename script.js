5ab37feee48fd01dba0d2acf209aff77a96e01eb
sam hockaday <samuel.hockaday@gmail.com>
2023-06-28 10:17:59 +0100

    Create player sprite and position.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-// const canvas = document.querySelector('canvas')
-// const c = canvas.getContext('2d')
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = 400;
+canvas.height = 500;
 
 const block = document.getElementById("block");
 const gap = document.getElementById("gap");
@@ -7,32 +10,41 @@ const gap = document.getElementById("gap");
 gap.addEventListener('animationiteration', () => {
     let random = -((Math.random() * 200) + 150);
     gap.style.right = random + "px";
-
 });
-//CREATE PLAYER SPRITE IN JS. 
-class Sprite {
-    constructor(position) {
-        this.position
-    }
-    //what should the sprite look like? 
-    draw() {
-        c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, 30, 30)
-
-    }
-
-}
 
 
-const player = new Sprite({
-    x: 0,
-    y: 0
-})
 
-player.draw();
 
-console.log(player)
-console.log("HELLO WORLD");
+
+
+
+
+
+
+// //CREATE PLAYER SPRITE IN JS. 
+// class Sprite {
+//     constructor(position) {
+//         this.position
+//     }
+//     //what should the sprite look like? 
+//     draw() {
+//         c.fillStyle = 'red'
+//         c.fillRect(this.position.x, this.position.y, 30, 30)
+
+//     }
+
+// }
+
+
+// const player = new Sprite({
+//     x: 0,
+//     y: 0
+// })
+
+// player.draw();
+
+// console.log(player)
+// console.log("HELLO WORLD");
 
 
 
