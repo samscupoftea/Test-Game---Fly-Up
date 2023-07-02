f0034caddba0c55a826b9e3c0c7c93d895d5213f
sam hockaday <samuel.hockaday@gmail.com>
2023-07-02 12:31:13 +0100

    Added Game over changed code

--- script.js
+++ script.js
@@ -1,112 +1,85 @@
-// const canvas = document.querySelector('canvas')
-// const c = canvas.getContext('2d')
+// // const canvas = document.querySelector('canvas')
+// // const c = canvas.getContext('2d')
 
 
 
-const block = document.getElementById("block");
-const gap = document.getElementById("gap");
-const character = document.getElementById("sprite");
-let interval;
-let both = 0;
+// const block = document.getElementById("block");
+// const gap = document.getElementById("gap");
+// const character = document.getElementById("sprite");
+// let interval;
+// let both = 0;
 
-function moveLeft() {
-    const left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
-    character.style.left = left - 2 + "px";
-}
+// function moveLeft() {
+//     const left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+//     character.style.left = left - 2 + "px";
+// }
 
-function moveRight() {
-    const left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
-    character.style.left = left + 2 + "px";
-}
+// function moveRight() {
+//     const left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+//     character.style.left = left + 2 + "px";
+// }
 
-document.addEventListener("keydown", event => {
-    if (both == 0) {
-        both++;
-        if (event.key === "ArrowLeft") {
-            interval = setInterval(moveLeft, 1);
-        }
-        if (event.key === "ArrowRight") {
-            interval = setInterval(moveRight, 1);
-        }
-    }
-});
-
-document.addEventListener("keyup", event => {
-    clearInterval(interval);
-    both = 0;
-});
-
-
-gap.addEventListener('animationiteration', () => {
-    let random = -((Math.random() * 200) + 150);
-    gap.style.right = random + "px";
-});
-
-// setInterval(function () {
-//     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-//     let blockTop = parseInt(window.getComputedStyle(block).getPropertyValue("top"));
-//     let blockBottom = blockTop + parseInt(window.getComputedStyle(block).getPropertyValue("height"));
-
-//     if (
-//         characterTop <= blockBottom &&
-//         characterTop >= blockTop &&
-//         (character.style.left >= block.offsetLeft && character.style.left <= block.offsetLeft + block.offsetWidth)
-//     ) {
-//         // Game over condition: character hits the blocks
-//         clearInterval(interval);
-//         alert("Game over!");
-//         // Reset character position or perform other game over actions
+// document.addEventListener("keydown", event => {
+//     if (both == 0) {
+//         both++;
+//         if (event.key === "ArrowLeft") {
+//             interval = setInterval(moveLeft, 1);
+//         }
+//         if (event.key === "ArrowRight") {
+//             interval = setInterval(moveRight, 1);
+//         }
 //     }
-// }, 10);
+// });
+
+// document.addEventListener("keyup", event => {
+//     clearInterval(interval);
+//     both = 0;
+// });
 
 
-
-setInterval(function () {
-    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-
-    let blockBottom = parseInt(window.getComputedStyle(block).getPropertyValue("bottom"));
-    let gapTop = parseInt(window.getComputedStyle(gap).getPropertyValue("top"));
-    let cTop = -(500 - characterTop);
-    if (((blockBottom < 20) && (blockBottom > -50) && ((cTop < gapTop) || (cTop > gapTop + 130)))) {
-        alert("Game over. Score: " + (counter - 1));
-        character.style.top = 100 + "px";
-        counter = 0;
-    }
-}, 10);
+// gap.addEventListener('animationiteration', () => {
+//     let random = -((Math.random() * 200) + 150);
+//     gap.style.right = random + "px";
+// });
 
 
 
 
 
-character = document.getElementById('character').getBoundingClientRect();
-let characterTop = character.top;
-let characterLeft = character.left;
-let characterRight = character.right
-let characterBottom = character.bottom
+// character = document.getElementById('sprite').getBoundingClientRect();
+// const characterTop = character.top;
+// const characterLeft = character.left;
+// const characterRight = character.right;
+// const characterBottom = character.bottom;
 
-block = document.getElementById('block').getBoundingClientRect();
-let blockTop = block.top;
-let blockLeft = block.left;
-let blockRight = block.right
-let blockBottom = block.bottom
+// block = document.getElementById('block').getBoundingClientRect();
+// const blockTop = block.top;
+// const blockLeft = block.left;
+// const blockRight = block.right;
+// const blockBottom = block.bottom;
 
-if ((blockTop > characterTop && blockTop < characterBottom) || (blockBottom > characterTop && blockBottom < characterBottom)) {
-    let verticalMatch = true
-} else {
-    let verticalMatch = false
-}
+// let verticalMatch = false;
+// let horizontalMatch = false;
 
-if ((blockRight > characterLeft && blockRight < characterRight) || (blockLeft < characterRight && blockLeft > characterLeft)) {
-    let horizontalMatch = true
-} else {
-    let horizontalMatch = false
-}
+// if ((blockTop > characterTop && blockTop < characterBottom) || (blockBottom > characterTop && blockBottom < characterBottom)) {
+//     verticalMatch = true;
+//     console.log('HIT - Vertical');
+// }
 
-if (horizontalMatch && verticalMatch) {
-    let intersect = true
-} else {
-    let intersect = false
-}
+// if ((blockRight > characterLeft && blockRight < characterRight) || (blockLeft < characterRight && blockLeft > characterLeft)) {
+//     horizontalMatch = true;
+//     console.log('HIT - Horizontal');
+// }
+
+// if (horizontalMatch && verticalMatch) {
+//     let intersect = true;
+//     console.log('HIT - Intersection');
+// } else {
+//     let intersect = false;
+// }
+
+
+
 
 
 
@@ -167,3 +140,61 @@ if (horizontalMatch && verticalMatch) {
 
 
 
+
+const block = document.getElementById("block");
+const gap = document.getElementById("gap");
+const character = document.getElementById("sprite");
+let interval;
+let both = 0;
+
+function moveLeft() {
+    const left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+    character.style.left = left - 2 + "px";
+    checkCollision();
+}
+
+function moveRight() {
+    const left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+    character.style.left = left + 2 + "px";
+    checkCollision();
+}
+
+document.addEventListener("keydown", event => {
+    if (both == 0) {
+        both++;
+        if (event.key === "ArrowLeft") {
+            interval = setInterval(moveLeft, 1);
+        }
+        if (event.key === "ArrowRight") {
+            interval = setInterval(moveRight, 1);
+        }
+    }
+});
+
+document.addEventListener("keyup", event => {
+    clearInterval(interval);
+    both = 0;
+});
+
+gap.addEventListener("animationiteration", () => {
+    let random = -((Math.random() * 200) + 150);
+    gap.style.right = random + "px";
+});
+
+function checkCollision() {
+    const characterRect = character.getBoundingClientRect();
+    const blockRect = block.getBoundingClientRect();
+
+
+    if (
+        characterRect.top <= blockRect.bottom &&
+        characterRect.top >= blockRect.top &&
+        characterRect.left >= blockRect.left &&
+        characterRect.right <= blockRect.right
+    ) {
+        // Game over condition: character hits the blocks
+        clearInterval(interval);
+        alert("Game over!");
+        // Reset character position or perform other game over actions
+    }
+}
